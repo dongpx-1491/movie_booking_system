bf1f8942cdc3459f211bf206e8b362c10970ffd4
dongpx-1491 <camapanchay14092001@gmail.com>
2023-04-09 02:21:22 +0700

    fixed_layout

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,5 +2,8 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link_tree ../../../vendor/stylesheets .css
 //= link bootstrap.min.js
 //= link jquery.min.js
+//= link application.css
+//= link_tree ../../../vendor/fonts
